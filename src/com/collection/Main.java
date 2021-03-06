5086a81be4c5e275a2616b9f35288ac4eefcc1f4
Natalis <natalis200623@gmail.com>
2021-03-06 20:37:08 +0300

    Fourth commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 7; i++) {
-            System.out.println("Hello!");
+            stem.out.println("Hello!");
             Thread.sleep(2000);
         }
     }
