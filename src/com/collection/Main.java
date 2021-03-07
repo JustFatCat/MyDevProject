8c18171ca7217ac4eef1906546177c5da086d36a
Natalis <natalis200623@gmail.com>
2021-03-07 18:59:16 +0300

    Build should be success

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 7; i++) {
-            tem.out.println("Hello!");
+            System.out.println("Hello!");
             Thread.sleep(2000);
         }
     }
