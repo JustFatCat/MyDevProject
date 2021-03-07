0f8349eb853d61ef3c7e89b170f6cb24c7368553
Natalis <natalis200623@gmail.com>
2021-03-07 18:55:15 +0300

    Build should fail

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 7; i++) {
-            System.out.println("Hello!");
+            tem.out.println("Hello!");
             Thread.sleep(2000);
         }
     }
